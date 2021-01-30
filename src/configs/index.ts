c6ffedd371051c3fb3fc6e1e13e1472d2ffdc4e4
Vladislav <haskicbr@gmail.com>
2021-01-30 03:55:03 +0300

    configs

---
+++ src/configs/index.ts
@@ -0,0 +1,24 @@
+import { MainScene } from "../scenes";
+
+const gameConfig = {
+    type: Phaser.AUTO,
+    backgroundColor: '#181818',
+    "render.transparent": true,
+    "render.autoResize": true,
+    width: window.screen.width,
+    height: window.screen.height,
+    scene: MainScene,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            fps: 60,
+            gravity: { y: 0 },
+            debug: true
+        }
+    },
+    disableContextMenu: false
+}
+
+export {
+    gameConfig
+}
